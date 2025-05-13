fb51b3d0e9af68b2f585c7ee16f30925f576e3a5
kk926-ya5 <zou1175200861@gmail.com>
2025-05-13 23:49:23 +0800

    Site updated: 2025-05-13 23:49:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/12/51品茶/","2024/05/12/汤头条/"];function toRandomPost(){
+var posts=["2024/05/12/暗网禁区/","2024/05/12/51品茶/","2024/05/12/汤头条/","2024/05/12/海角乱伦社区/","2024/05/12/萝莉岛/","2024/05/12/麻豆视频/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
