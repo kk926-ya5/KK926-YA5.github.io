9d2fbd51241338eeee61ebc805b7780b18ab24b6
KK926-YA5 <zou1175200861@gmail.com>
2025-05-13 10:47:38 +0800

    Site updated: 2025-05-13 10:47:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/12/hello-world/"];function toRandomPost(){
+var posts=["2024/05/12/51品茶/","2024/05/12/汤头条/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
